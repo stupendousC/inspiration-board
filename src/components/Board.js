b2a92618420e9d851bb61f29d88a2695f96d3aec
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-13 11:54:32 -0800

    api able to retrieve all cards under boardURL

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,17 +8,34 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      boardURL: this.props.url + this.props.boardName,
+      error: "",
       cards: [],
     };
   }
 
+  componentDidMount() {
+    // console.log("<Board> mounted");
+
+    axios.get(this.state.boardURL + "/cardsxxx")
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      this.setState({ error: `Oh hell no!  ${error.message}`});
+    })
+  }
+
+
   render() {
     return (
       <div>
+        { this.state.error ? <h1>{this.state.error}</h1>: null}
+        
         Board
       </div>
     )
@@ -27,7 +44,8 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired,
 };
 
 export default Board;
