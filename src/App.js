96d84cbb5174b79209e145d4bc21284d9ef353ee
stupendousC <CarolineWuKaplan@gmail.com>
2019-12-14 15:02:22 -0800

    incorporated boardName into header text

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Board from './components/Board';
 
 class App extends Component {
+  state = { boardName: "StupendousC" }
+
   render() {
     return (
       <section>
         <header className="header">
           <h1 className="header__h1"><span className="header__text">Inspiration Board</span></h1>
+          <h1 className="header__h1"><span className="header__text boardName">{this.state.boardName}</span></h1>\
         </header>
 
         <Board
           baseUrl="https://inspiration-board.herokuapp.com"
           url="https://inspiration-board.herokuapp.com/boards/"
-          boardName={`StupendousC`}
-          // boardName={1}
+          boardName={this.state.boardName}
           />
 
       </section>
